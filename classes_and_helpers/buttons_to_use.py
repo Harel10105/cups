50890d40811010ff8e491ac39a15e2e05a3af080
HAREL <hl9163@bs.amalnet.k12.il>
2022-04-10 13:25:11 +0300

    Initial commit

---
+++ classes_and_helpers/buttons_to_use.py
@@ -0,0 +1,9 @@
+from classes_and_helpers.constants import *
+from classes_and_helpers.Button import *
+
+start_button = Button(START_BUTTON_X, START_BUTTON_Y, START_BUTTON_WIDTH, START_BUTTON_HEIGHT)
+new_round_button = Button(START_NEW_ROUND_X,START_NEW_ROUND_Y,START_NEW_ROUND_WIDTH,START_NEW_ROUND_HEIGHT)
+
+first_cup_button = Button(FIRST_CUP_START_X,DOWN_CUP_HEIGHT,CUP_WIDTH,CUP_HEIGHT)
+second_cup_button = Button(SECOND_CUP_START_X,DOWN_CUP_HEIGHT,CUP_WIDTH,CUP_HEIGHT)
+third_cup_button = Button(THIRD_CUP_START_X,DOWN_CUP_HEIGHT,CUP_WIDTH,CUP_HEIGHT)
